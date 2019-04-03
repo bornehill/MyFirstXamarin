97e09e7ee6ad356d1e72cd4b490b880052dc6a6a
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-03 00:03:06 -0600

    ListView with image

--- MyFirstXamarin/MyFirstXamarin/Model/Person.cs
+++ MyFirstXamarin/MyFirstXamarin/Model/Person.cs
@@ -8,17 +8,20 @@ namespace MyFirstXamarin.Model
     {
         public string Name { get; set; }
         public string Address { get; set; }
+        public string ImageSource { get; set; }
 
         public Person()
         {
             Name = "Test";
             Address = "Test address";
+            ImageSource = "punish.png";
         }
 
         public Person(string name, string addr)
         {
             Name = name;
             Address = addr;
+            ImageSource = "punish.png";
         }
     }
 }
