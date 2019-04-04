2be8eaafcbbf740b56db9deabeef7cb8c5cae29f
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-04 12:56:16 -0600

    Add subscribe message center

--- MyFirstXamarin/MyFirstXamarin/View/MainPage.xaml.cs
+++ MyFirstXamarin/MyFirstXamarin/View/MainPage.xaml.cs
@@ -34,5 +34,10 @@ namespace MyFirstXamarin
         {
             Navigation.PushAsync(new BehaviorPage());
         }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MessagePage());
+        }
     }
 }
---
+++ MyFirstXamarin/MyFirstXamarin/ViewModel/MessageVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyFirstXamarin.ViewModel
+{
+    class MessageVM
+    {
+        public ICommand ButtonClickedCommand { get; set; }
+
+        public MessageVM()
+        {
+            ButtonClickedCommand = new Command(HandleButtonClick);
+        }
+
+        private void HandleButtonClick()
+        {
+            MessagingCenter.Send<MessageVM>(this, "ButtonClicked");
+        }
+    }
+
+}
